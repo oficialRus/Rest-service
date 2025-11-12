6d75b6a64602077986280477d6aba2b7f758ee9b
RUSLAN_MOZH_AKAD <RUSLAN_MOZH2003>
2025-11-12 06:26:16 -0500

    ком на 11.12.

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"log/slog"
 	"os"
 	"rest-service/internal/config"
@@ -15,25 +16,29 @@ const (
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка загрузки переменных в ОС.")
+	}
 	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env))
+	log.Info("initializing server", slog.String("addres", cfg.Address))
+	log.Debug("logger debug mode enabled")
 
 }
 
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
+
 	switch env {
 	case envLocal:
 		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envDev:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envProd:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	}
+
 	return log
-
-}
-
-func init() {
-	godotenv.Load()
 }
