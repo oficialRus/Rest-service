480b70a53732cf4114407692d77682f02fa90e85
RUSLAN_MOZH_AKAD <RUSLAN_MOZH2003>
2025-11-26 03:24:40 -0500

    Дописан тест на TestHttpHandler

--- internal/http-server/handlers/url/saving/save_test.go
+++ internal/http-server/handlers/url/saving/save_test.go
@@ -2,12 +2,20 @@ package saving
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"log/slog"
 	"net/http/httptest"
 	"rest-service/internal/http-server/handlers/url/saving/mocks"
 	"testing"
+
+	"github.com/stretchr/testify/mock"
 )
+
+type ResponseTest struct {
+	Status string `json:"status"`
+	Alias  string `json:"alias"`
+}
 
 func TestHttpHandler(t *testing.T) {
 	log := slog.New(slog.NewTextHandler(io.Discard, nil))
@@ -17,6 +25,22 @@ func TestHttpHandler(t *testing.T) {
 
 	testURLSaver := mocks.NewURLSaver(t)
 	handler := New(log, testURLSaver)
+	testURLSaver.On("SaveURL", "https://google.com", mock.Anything).Return(int64(1), nil)
 	handler.ServeHTTP(record, req)
+	if record.Code != 200 {
+		t.Errorf("expected 200, got %d", record.Code)
+	}
+	respBody := record.Body.String()
+	var response ResponseTest
+	err := json.Unmarshal([]byte(respBody), &response)
+	if err != nil {
+		t.Errorf("ailed to unmarshal response: %v", err)
 
+	}
+	if response.Status != "OK" {
+		t.Errorf("expected status OK,got %s", response.Status)
+	}
+	if response.Alias == "" {
+		t.Errorf("alias must be not empty")
+	}
 }
