1798e7b00d72852df98a7d120cdf04ad61b870dc
RUSLAN_MOZH_AKAD <RUSLAN_MOZH2003>
2025-11-28 06:11:59 -0500

    Добавил начало Delete

--- internal/http-server/handlers/url/delete/remove.go
+++ internal/http-server/handlers/url/delete/remove.go
@@ -1,1 +1,24 @@
 package remove
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type RemoveURL interface {
+	DeleteURL(alias string) error
+}
+
+func New(log *slog.Logger, urlDelete RemoveURL) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "internal.http-server.handlers.url.delete"
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+		resURL, err := urlDelete.DeleteURL(alias)
+	}
+
+}
